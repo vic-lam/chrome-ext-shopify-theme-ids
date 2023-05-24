16b43885b219ae9f6aaee9afd6480e40e056cec1
Vic Lam <lam.vic.k@gmail.com>
2023-05-24 14:11:39 -0400

    AdminPage:Modal:Account for new request url

--- src/js/adminPage.js
+++ src/js/adminPage.js
@@ -1,35 +1,37 @@
-(function() {
-  
-   function loadModal() {
-    var message = '';
-    var xmlUrl = 'https://' + window.location.host + '/admin/themes.xml';
-    var req = new XMLHttpRequest();
-    req.open('GET', xmlUrl);
-    req.setRequestHeader('Content-Type', 'application/json');
+(function () {
+  function loadModal() {
+    const newStoreHandleRegex = "https:\/\/admin\.shopify\.com\/store\/([^;]*)\/";
+    const newStoreHandle = window.location.href.match(newStoreHandleRegex)?.[1];
+    const newDomain = `https://admin.shopify.com/store/${newStoreHandle}/themes.xml`
+    const oldDomain = `https://${window.location.host}/admin/themes.xml`;
+    const xmlUrl = newStoreHandle ? newDomain : oldDomain;
+    const req = new XMLHttpRequest();
+    req.open("GET", xmlUrl);
+    req.setRequestHeader("Content-Type", "application/json");
     req.onload = () => {
       if (req.status === 200) {
         window.__CESTI.modal.build();
-  
+
         // Add Themes to Modal
-        var themesArray = window.__CESTI.admin.returnThemesArray(req.response);
-        for (var i = 0; i < themesArray.length; i++) {
-          var themeData = themesArray[i];
+        const themesArray = window.__CESTI.admin.returnThemesArray(req.response);
+        for (let i = 0; i < themesArray.length; i++) {
+          const themeData = themesArray[i];
           if (themeData) {
-            var themeNode = window.__CESTI.admin.returnThemeNode(themeData);
+            const themeNode = window.__CESTI.admin.returnThemeNode(themeData);
             window.__CESTI.modal.els.content.appendChild(themeNode);
           }
         }
 
         window.__CESTI.modal.open();
       } else {
-        alert('Something messed up');
+        alert("Something messed up");
       }
-    }
-  
+    };
+
     req.send();
   }
 
-  if (document.querySelector('#vpvModal')) {
+  if (document.querySelector("#vpvModal")) {
     window.__CESTI.modal.remove();
   } else {
     loadModal();
