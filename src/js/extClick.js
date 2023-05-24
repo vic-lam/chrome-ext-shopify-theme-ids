c30707da424ff7204e5a93f12b743772e410da37
Vic Lam <lam.vic.k@gmail.com>
2023-05-24 14:10:31 -0400

    Click:Recognize new admin url structure

--- src/js/extClick.js
+++ src/js/extClick.js
@@ -1,21 +1,22 @@
 (function () {
-
   function injectScript(file, node) {
-    var th = document.getElementsByTagName(node)[0];
-    var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('src', file);
+    const th = document.getElementsByTagName(node)[0];
+    const s = document.createElement("script");
+    s.setAttribute("type", "text/javascript");
+    s.setAttribute("src", file);
     th.appendChild(s);
   }
-  
-  injectScript(chrome.extension.getURL('/src/js/helpers/global.js'), 'body');
-  injectScript(chrome.extension.getURL('/src/js/helpers/modal.js'), 'body');
-  
-  if (window.location.href.includes('myshopify.com/admin')) {
-    injectScript(chrome.extension.getURL('/src/js/helpers/admin.js'), 'body');
-    injectScript(chrome.extension.getURL('/src/js/adminPage.js'), 'body');
+
+  injectScript(chrome.extension.getURL("/src/js/helpers/global.js"), "body");
+  injectScript(chrome.extension.getURL("/src/js/helpers/modal.js"), "body");
+
+  const { href } = window.location;
+  const isAdminPage =
+    href.includes("myshopify.com") || href.includes("admin.shopify.com");
+  if (isAdminPage) {
+    injectScript(chrome.extension.getURL("/src/js/helpers/admin.js"), "body");
+    injectScript(chrome.extension.getURL("/src/js/adminPage.js"), "body");
   } else {
-    injectScript(chrome.extension.getURL('/src/js/nonAdminPage.js'), 'body');
+    injectScript(chrome.extension.getURL("/src/js/nonAdminPage.js"), "body");
   }
-   
 })();
